ef0af9a088335efe5c65f9d899f39683d4c84f0a
Joep-vink <78424962+Joep-vink@users.noreply.github.com>
2023-01-10 10:55:10 +0100

    small changes

--- Silent Night/Assets/Scripts/JumpScare.cs
+++ Silent Night/Assets/Scripts/JumpScare.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
 public class JumpScare : MonoBehaviour
 {
+    public Volume volume;
+    public VolumeProfile JpProfile, NormalProfile;
     private Animator JumpscareAnim;
 
     private void Awake()
@@ -14,11 +17,13 @@ public class JumpScare : MonoBehaviour
 
     public void StartJumpScare()
     {
+        volume.profile = JpProfile;
         JumpscareAnim.Play("JumpScare");
     }
 
     public void EndOfJumpscare()
     {
+        volume.profile = NormalProfile;
         SceneManager.LoadScene(2);
     }
 }
